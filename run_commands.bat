c3591d91b4cf4df83e7cbfe546e3705cc91c3192
CryptoCommander007 <rivantped@gmail.com>
2024-06-25 08:06:40 -0400

    PROYECTOFOROIMPORTACIONES

---
+++ run_commands.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "PROYECTOFOROIMPORTACIONES"
+git branch -M main
+git remote add origin https://github.com/CryptoCommander007/PROYECTOFOROIMPORTACIONES.git
+git push -u origin main
